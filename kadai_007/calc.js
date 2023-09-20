a6239cb378a25618203027c9f5b963657830a251
tsasagawa0510 <mail@tsubasa-sasagawa.com>
2023-09-20 19:59:09 +0900

    kadai007

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -2,4 +2,14 @@ let num
 
 num = Math.floor(Math.random() *20);
 
-console.log(num);
+
+
+if (num % 3 === 0 && num % 5 === 0 ) {
+  console.log('変数' + num + 'は3と5の倍数です');
+} else if (num % 3 === 0 ) {
+  console.log('変数' + num + 'は3の倍数です');
+} else if (num % 5 === 0) {
+  console.log('変数' + num + 'は5の倍数です');
+} else {
+  console.log(num);
+} 
